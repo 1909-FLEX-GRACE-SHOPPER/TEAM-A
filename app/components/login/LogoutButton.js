e0b1ed8a7836587f92c222ed724ae0766d7b7234
Alexandra Morozoff <a.j.morozoff@gmail.com>
2020-01-26 14:47:49 -0500

    added logout button

--- app/components/login/LogoutButton.js
+++ app/components/login/LogoutButton.js
@@ -24,3 +24,5 @@ const LogoutButton = (props) => {
         </div>
     )
 }
+
+export default LogoutButton;
--- app/redux/user.js
+++ app/redux/user.js
@@ -84,7 +84,7 @@ export const createGuestAndCart = () => {
   }
 }
 
-const userLogout = () => {
+export const userLogout = () => {
   return (dispatch, getState, {axios}) => {
       return axios.put('/auth/login', {logout: true})
           .then(() => dispatch(setUser('')))
