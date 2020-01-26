b95681999699dd104f7cc65eb580d57a16c38804
Lachlan Glen <lachlanjglen@gmail.com>
2020-01-26 14:01:23 -0500

    All Orders & Single Order component built; store updated

--- app/redux/index.js
+++ app/redux/index.js
@@ -3,14 +3,14 @@ import { productsReducer } from './products'
 import { singleProductReducer } from './singleProduct'
 import { userReducer } from './user'
 import { cartReducer } from './cart'
-import { orderReducer } from './order'
+import { ordersByUserReducer } from './ordersByUser'
 
 const appReducer = combineReducers({
   products: productsReducer,
   user: userReducer,
   selectedProduct: singleProductReducer,
   cart: cartReducer,
-  order: orderReducer,
+  ordersByUser: ordersByUserReducer,
 })
 
 export default appReducer;
--- server/api/order.js
+++ server/api/order.js
@@ -7,23 +7,49 @@ router.use(express.json());
 
 //return a single order (and items) by id
 router.get('/:orderId', (req, res, next) => {
-    Order.findOne({
-        where: {
-            id: req.params.orderId,
-        },
-        include: [{
-            model: OrderItem,
-        }],
-    })
+  Order.findOne({
+    where: {
+      id: req.params.orderId,
+    },
+    include: [{
+      model: OrderItem,
+    }],
+  })
     .then(result => {
-        if (result) {
-            return res.status(200).send(result);
-        }
-        res.status(404).send('Not found');
+      if (result) {
+        return res.status(200).send(result);
+      }
+      res.status(404).send('Not found');
     })
     .catch(e => {
-        res.status(400).send('Invalid request');
-        next(e);
+      res.status(400).send('Invalid request');
+      next(e);
+    })
+});
+
+//return all orders by userId
+
+router.get('/byuser/:userId', (req, res, next) => {
+  const { userId } = req.params;
+  // console.log('userId: ', userId)
+  // console.log('typeof userId: ', typeof (userId))
+  Order.findAll({
+    where: {
+      userId: parseInt(userId),
+    },
+    include: [{
+      model: OrderItem,
+    }],
+  })
+    .then(results => {
+      if (results) {
+        return res.status(200).send(results);
+      }
+      res.status(200).send([]);
+    })
+    .catch(e => {
+      res.status(400).send('Invalid request for orders by userId');
+      next(e);
     })
 });
 
@@ -31,83 +57,83 @@ router.get('/:orderId', (req, res, next) => {
 //example 1: /api/order
 //example 2: /api/order?sort=status&dir=ASC
 router.get('/', (req, res, next) => {
-    Order.findAll({
-        include: [{
-            model: OrderItem,
-        }],
-        order: [
-            [req.query.sort || 'createdAt', req.query.dir || 'ASC']
-        ],
-    })
+  Order.findAll({
+    include: [{
+      model: OrderItem,
+    }],
+    order: [
+      [req.query.sort || 'createdAt', req.query.dir || 'ASC']
+    ],
+  })
     .then(results => {
-        res.status(200).send(results);
+      res.status(200).send(results);
     })
     .catch(e => {
-        res.status(400).send('Invalid request');
-        next(e);
+      res.status(400).send('Invalid request');
+      next(e);
     })
 });
 
 //create new order. UserId is required
 router.post('/', (req, res, next) => {
-    if (!req.body.userId) {
-        return res.status(400).send('Invalid request; userId required');
-    }
-    Order.create({
-        status: req.body.status || 'pending',
-        userId: req.body.userId,
-    })
+  if (!req.body.userId) {
+    return res.status(400).send('Invalid request; userId required');
+  }
+  Order.create({
+    status: req.body.status || 'pending',
+    userId: req.body.userId,
+  })
     .then(created => {
-        res.status(201).send(created);
+      res.status(201).send(created);
     })
     .catch(e => {
-        res.status(400).send('Invalid request');
-        next(e);
+      res.status(400).send('Invalid request');
+      next(e);
     })
 });
 
 //Update an order
 //As of now, the only field on an order that can be modified is status
 router.put('/:orderId', (req, res, next) => {
-    Order.update(
-        {
-            status: req.body.status 
-        },
-        {
-            where: {
-                id: req.params.orderId
-            },
-            returning: true,
-        }
-    )
+  Order.update(
+    {
+      status: req.body.status
+    },
+    {
+      where: {
+        id: req.params.orderId
+      },
+      returning: true,
+    }
+  )
     .then(updated => {
-        if (updated[0]) {
-            return res.status(200).send(updated[1]);
-        }
-        res.status(404).send('Not found');
+      if (updated[0]) {
+        return res.status(200).send(updated[1]);
+      }
+      res.status(404).send('Not found');
     })
     .catch(e => {
-        res.status(400).send('Invalid request');
-        next(e);
+      res.status(400).send('Invalid request');
+      next(e);
     })
 });
 
 //delete order. This also deletes all associated orderItems
 router.delete('/:orderId', (req, res, next) => {
-    Order.destroy({
-        where: {
-            id: req.params.orderId
-        },
-        include: [{
-            model: OrderItem,
-        }],
-    })
+  Order.destroy({
+    where: {
+      id: req.params.orderId
+    },
+    include: [{
+      model: OrderItem,
+    }],
+  })
     .then(() => {
-        res.status(204).send(`deleted order ${req.params.orderId}`);
+      res.status(204).send(`deleted order ${req.params.orderId}`);
     })
     .catch(e => {
-        res.status(400).send('Invalid request');
-        next(e);
+      res.status(400).send('Invalid request');
+      next(e);
     })
 });
 
