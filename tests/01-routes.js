c29e2ca2fd221011153320200b915824b8a1385c
Alexandra Morozoff <a.j.morozoff@gmail.com>
2020-01-31 23:24:35 -0500

    fixes for routes test

--- tests/01-routes.js
+++ tests/01-routes.js
@@ -1,57 +1,78 @@
 const { expect } = require('chai');
 const { connection }  = require('../db/');
 const { Product, User } = require('../db');
+const faker = require('faker');
 const sinon = require('sinon');
 const app = require('../server');
 const agent = require('supertest')(app);
 
 describe('API Routes', async() => {
-    beforeEach(async() => {
+    before(async() => {
         await connection.sync({ force: true });
     });
 
-    afterEach(async() => {
+    after(async() => {
         await connection.sync({ force: true });
     });
     
     describe('GET /api/products', async() => {
-        const product1 = { name: 'Tasty Cotton Pizza', inventory: 3, price: 25.00 };
-        const product2 = { name: 'Intelligent Plastic Mouse', inventory: 5, price: 10.00 };
-        const product3 = { name: 'Generic Soft Tuna', inventory: 2, price: 55.00 };
+        let productList = Array(3);
+        before(async() => {
+            //generate data
+            for (let i = 0; i < 3; i++) {
+                productList[i] = {
+                  name: faker.commerce.productName(),
+                  description: faker.lorem.sentence(),
+                  inventory: Math.round(Math.random() * 2000),
+                  price: faker.commerce.price(1.00, 99.99, 2),
+                }
+            };
 
-        await Promise.all([
-            Product.create(product1),
-            Product.create(product2),
-            Product.create(product3)
-        ]);
+            await Promise.all(productList.map(product => Product.create({ ...product })));
+        });
 
         it('GET / responds with all products', async() => {
-            const response = await agent.get('/api/products').expect(200);
-            expect(response.body).to.deep.equal([
-                { id: 1, name: 'Tasty Cotton Pizza', inventory: 3, price: 25.00 },
-                { id: 2, name: 'Tasty Rubber Ball', inventory: 50, price: 5.00 }
-            ]);
-            expect(Product.findAll.calledOnce).to.be.equal(true);
+
+            const responseBody = (await agent.get('/api/products')).body;
+            expect(responseBody.length).to.equal(3);
+            responseBody.forEach((prod, idx) => {
+                expect(prod.name).to.equal(productList[idx].name);
+                expect(prod.description).to.equal(productList[idx].description);
+                expect(prod.inventory).to.equal(productList[idx].inventory);
+                expect(prod.price).to.equal(productList[idx].price);
+            });
         });
 
         it('GET /:productId responds with specified product', async() => {
-            const response = await agent.get('/api/products/1').expect(200);
-            expect(response.body).to.deep.equal([
-                { id: 1, name: 'Tasty Cotton Pizza', inventory: 3, price: 25.00 }
-            ]);
+
+            let randomIdx = Math.floor(Math.random() * 10) % 3 || 1;
+            const response = (await agent.get(`/api/products/${randomIdx}`)).body;
+            expect(response.id).to.equal(randomIdx);
+            expect(response.name).to.equal(productList[randomIdx - 1].name);
+            expect(response.description).to.equal(productList[randomIdx - 1].description);
+            expect(response.inventory).to.equal(productList[randomIdx - 1].inventory);
+            expect(response.price).to.equal(productList[randomIdx - 1].price);
+            
         });
 
     });
 
     describe('User Routes', async() => {
+        let userList = [
+            { firstName: 'Will', lastName: 'Apple', email: 'FreshPrince@gmail.com', password: 'abc'},
+            { firstName: 'John', lastName: 'Doe', email: 'Jdoe@gmail.com', password: 'xyz'},
+            { firstName: 'Homer', lastName: 'Zebra', email: 'donuts@gmail.com', password: '123' },
+        ];
+        before(async() => {
+            await Promise.all(userList.map(user => User.create({...user})));
+        });
+
         it('GET /api/user responds with all users', async() => {
-            await Promise.all([
-                User.create({ firstName: 'Will', lastName: 'Smith', email: 'FreshPrince@gmail.com', password: 'abc'}),
-                User.create({ firstName: 'Homer', lastName: 'Simpson', email: 'donuts@gmail.com', password: '123' }),
-            ]);
-            const apiResponse = await agent.get('/api/user').expect(200);
-            //test goes here
-            expect(true).to.equal(true);
+            const responseBody = (await agent.get('/api/user')).body;
+            expect(responseBody.length).to.equal(userList.length);
+
+            //by default, the /user API returns sorted by LastName
+            expect(responseBody[0].lastName).to.equal('Apple');
         });
     });
 });
