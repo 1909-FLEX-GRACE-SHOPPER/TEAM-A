9881cec335310dbd5b72f838616f16cb33d7ad10
Alexandra Morozoff <a.j.morozoff@gmail.com>
2020-02-02 15:39:22 -0500

    basic framework of component

--- app/components/Pagination.js
+++ app/components/Pagination.js
@@ -1,28 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const useStyles = makeStyles(theme => ({
     wrapper: {
         margin: theme.spacing(1),
-        display: "inline-block"
+        display: "inline-block",
+        width: "320px"
     },
     paper: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-evenly"
+        justifyContent: "space-evenly",
+        alignItems: "stretch",
     },
-    form: {
-        width: "100%",
+    arrow: {
+        flexGrow: 1,
     },
-    sharedGrid: {
-        marginLeft: theme.spacing(2),
+    page: {
+
     }
 }));
 
 const Pagination = (props) => {
+    const [page, setPage] = useState(1);
+    const [enabledForward, setForward] = useState(true);
+    const [enabledBack, setBack] = useState(false);
+    const dispatch = useDispatch();
+    const classes = useStyles();
 
-}
+    return (
+        <Container component='div'>
+            <CssBaseline />
+            <div className={classes.paper}>
+                {/* Icon back arrow */}
+                {page}
+                {/* Icon forward arrow */}
+            </div>
+        </Container>
+    )
+};
 
 export default withRouter(Pagination);
