c8edb8c7142bb314dc3a932103f9a6d13bdd23ef
Alexandra Morozoff <a.j.morozoff@gmail.com>
2020-02-01 21:42:02 -0500

    added delete to product edit page; styling udpates

--- app/components/EditProduct.js
+++ app/components/EditProduct.js
@@ -1,12 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { updateProduct } from '../redux/products';
+import { updateProduct, deleteProduct } from '../redux/products';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        marginTop: theme.spacing(4),
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+    },
+    form: {
+        width: "100%",
+    },
+    sharedGrid: {
+        marginLeft: theme.spacing(2),
+    }
+}));
 
 export default function EditProduct(props) {
     const pageId = props.match.params.id;
     const dispatch = useDispatch();
+    const classes = useStyles();
 
     const user = useSelector((state) => state.user);
     const products = useSelector((state) => state.products);
@@ -24,13 +47,18 @@ export default function EditProduct(props) {
             setName(match.name);
             setDescription(match.description);
             setPrice(match.price);
-            setInventory(math.inventory);
+            setInventory(match.inventory);
         }
     });
 
-    const updateHandle = () => {
+    const handlePUT = () => {
         dispatch(updateProduct({ name, description, price, inventory }, product.id));
         props.history.push(`/products/${product.id}`);
+    }
+
+    const deleteHandle = () => {
+        dispatch(deleteProduct(product.id));
+        props.history.push('/');
     }
 
     if (!product) {
@@ -51,60 +79,89 @@ export default function EditProduct(props) {
     
 
     return (
-        <div 
-            id="edit-form"
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between"
-            }}
-        >
-            <h1>
-                Edit Product
-            </h1>
-            <TextField
-                id="name"
-                label="Product Name"
-                variant="outlined"
-                value={name}
-                onChange={(ev) => setName(ev.target.value)}
-            >
-                {name}
-            </TextField>
-            <TextField
-                id="description"
-                label="Product Description"
-                variant="outlined"
-                value={description}
-                onChange={(ev) => setDescription(ev.target.value)}
-            >
-                {description}
-            </TextField>
-            <TextField
-                id="price"
-                label="Product Price"
-                variant="outlined"
-                value={price}
-                onChange={(ev) => setPrice(ev.target.value)}
-            >
-                {price}
-            </TextField>
-            <TextField
-                id="inventory"
-                label="Product Inventory"
-                variant="outlined"
-                value={inventory}
-                onChange={(ev) => setInventory(ev.target.value)}
-            >
-                {inventory}
-            </TextField>
-            <Button
-                variant="outlined"
-                color="primary"
-                onClick={updateHandle}
-            >
-                Update
-            </Button>
-        </div>
+        <Container component='div'>
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography component="h1">
+                    Edit Product
+                </Typography>
+                <form className={classes.form}>
+                    <Grid container spacing={1}>
+                        <Grid item xs={12}>
+                            <TextField
+                                id="name"
+                                label="Product Name"
+                                variant="outlined"
+                                value={name}
+                                fullWidth
+                                margin="normal"
+                                onChange={(ev) => setName(ev.target.value)}
+                            >
+                                
+                                {name}
+                            </TextField>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                id="description"
+                                label="Product Description"
+                                variant="outlined"
+                                value={description}
+                                fullWidth
+                                margin="normal"
+                                onChange={(ev) => setDescription(ev.target.value)}
+                            >
+                                {description}
+                            </TextField>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                id="price"
+                                label="Product Price"
+                                variant="outlined"
+                                value={price}
+                                margin="normal"
+                                onChange={(ev) => setPrice(ev.target.value)}
+                                InputProps={{
+                                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                                }}
+                            >
+                                {price}
+                            </TextField>
+                            <TextField
+                                id="inventory"
+                                label="Product Inventory"
+                                variant="outlined"
+                                value={inventory}
+                                margin="normal"
+                                className={classes.sharedGrid}
+                                onChange={(ev) => setInventory(ev.target.value)}
+                            >
+                                {inventory}
+                            </TextField>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                margin="normal"
+                                onClick={handlePUT}
+                            >
+                                Update
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                margin="normal"
+                                className={classes.sharedGrid}
+                                onClick={deleteHandle}
+                            >
+                                Delete Product
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
     )
 };
