43d7369cad1aadfb013b0988f169bd151d716f97
Kalvin Zhao <kalvinz61@gmail.com>
2020-02-04 21:17:47 -0500

    update addReview thunk

--- app/redux/review.js
+++ app/redux/review.js
@@ -1,29 +1,10 @@
 import axios from 'axios'
 
-const ADD_REVIEW = 'ADD_REVIEW'
-
-const addReview = (review) => {
-    return {
-        type: ADD_REVIEW,
-        review
-    }
-}
-
-export const reviewReducer = (state = [], action) => {
-    switch (action.type) {
-        case ADD_REVIEW:
-            state = [...state.review, action.review]
-            return state;
-        default:
-            return state;
-    }
-}
-//thunk to fetch reviews
-
-export const addReview = () => {
+//thunk to add a review
+export const addReview = (review, productId) => {
     return (dispatch) => {
-        axios.post('/api/review', (productId))
-            .then(reviews => dispatch(addReview(reviews)))
+        axios.post('/api/review', (review))
+            .then(review => dispatch(addReview(review, productId)))
             .catch(e => console.log(e))
     }
 }
