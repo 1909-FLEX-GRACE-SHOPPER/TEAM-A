59bfbd09e9289da2abca2235056fa819bbd081ab
Sameehan Patel <sp4315@nyu.edu>
2020-02-05 20:24:42 -0500

    beginning of the end

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,13 +6,24 @@ const theme = createMuiTheme({
   palette: {
     primary:{
       main: '#0B2545',
+    },
+    secondary: {
+      main: '#9FD8CB',
     }
   },
+  props: {
+    MuiButtonBase: {
+      disableRipple: true, 
+    },
+  },
+  transitions: {
+    // So we have `transition: none;` everywhere
+    create: () => 'none',
+  },
   typography: {
-    fontFamily: "Nunito Sans, Roboto, sans-serif",
+    fontFamily:[ "-apple - system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans- serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"].join(",")
   },
   contrastThreshold: 3,
-
 });
 
 
